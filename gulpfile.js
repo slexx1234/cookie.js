ec358d8cffc9ff39cc8610721e3b1fd6225b2231
Alex5646 <catia.petrowi4@yandex.ru>
2016-04-04 16:41:32 +1000

    Assembly system

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp       = require('gulp'),
+    plumber    = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify     = require('gulp-uglify'),
+    ts         = require('gulp-typescript'),
+    rename     = require('gulp-rename');
+
+gulp.task('js', function() {
+    gulp.src('./cookie.ts')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(ts())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('min', function() {
+    gulp.src('./cookie.js')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./cookie.ts', ['js', 'min']);
+});
+
+gulp.task('default', ['watch', 'js', 'min']);
